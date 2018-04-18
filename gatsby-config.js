f09c95537f7f7c2a94135b1d19eb5edb6e99ce0a
Jason L Perry <jason@suncoast.io>
2018-04-17 21:15:54 -0400

    CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Suncoast Developers Guild',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+  ],
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const ContactPage = () => (
   <div className="ContactPage">
-    <h2>Contact Form & MAp</h2>
+    <h2>Contact Form & Map</h2>
+    <p>Question? Comments? Just want to chat and say "Hello"?</p>
+    <p>Form: name/email/phone/message</p>
+    <p>Interested in:</p>
+    <ul>
+      <li>Learning web development</li>
+      <li>Demo Day & hiring events</li>
+      <li>Community events</li>
+      <li>Donating or becoming a sponsor</li>
+      <li>Volunteering</li>
+      <li>Hiring a developer</li>
+    </ul>
   </div>
 )
 
