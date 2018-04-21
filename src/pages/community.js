8326e2722e46cb79dfd69a5d0690dbfdfbea86b7
Jason L Perry <jason@suncoast.io>
2018-04-21 13:44:37 -0400

    Update
    A lazy commit.

--- src/pages/community.js
+++ src/pages/community.js
@@ -4,29 +4,12 @@ import Organization from '../components/Organization'
 
 const CommunityPage = ({ data }) => (
   <div className="CommunityPage">
-    <div className="Events">
-      <h2>Upcoming Events</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <span className="month">May</span>
-              <span className="day">24</span>
-            </td>
-            <td>
-              <h3>
-                <a href="https://www.meetup.com/suncoast-developers-guild/events/248468799/">
-                  Kid's Academy with SDG Jr.
-                </a>
-              </h3>
-              <h4>
-                Hosted by: <strong>Suncoast Developers Guild</strong>
-              </h4>
-            </td>
-            <td>Monday @ 6:30 PM</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="wrap">
+      <h2>Community</h2>
+      <p>
+        Suncoast Developers Guild is proud to help connect dozens of local tech
+        organizations.
+      </p>
       <div className="organizations">
         <ul>
           {data.allOrganizationsJson.edges.map(({ node }) => (
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,58 +2,63 @@ import React from 'react'
 
 const ContactPage = () => (
   <div className="ContactPage">
-    <h2>Contact Form & Map</h2>
-    <p>Question? Comments? Just want to chat and say "Hello"?</p>
-    <p>Form: name/email/phone/message</p>
-    <form action="">
-      <p>
-        <label htmlFor="">Name</label>
-        <input type="text" />
-      </p>
-      <p>
-        <label htmlFor="">Email Address</label>
-        <input type="email" />
-      </p>
-      <p>
-        <label htmlFor="">Telephone Number</label>
-        <input type="tel" />
-      </p>
-      <p>
-        <label htmlFor="">Message</label>
-        <textarea name="" id="" cols="30" rows="10" />
-      </p>
-      <p>I'm interested in:</p>
-      <ul>
-        <li>
-          <input type="checkbox" /> <label>Learning web development</label>
-        </li>
-        <li>
-          <input type="checkbox" /> <label>Demo Day & hiring events</label>
-        </li>
-        <li>
-          <input type="checkbox" /> <label>Community events</label>
-        </li>
-        <li>
-          <input type="checkbox" />{' '}
-          <label>Donating or becoming a sponsor</label>
-        </li>
-        <li>
-          <input type="checkbox" /> <label>Volunteering</label>
-        </li>
-        <li>
-          <input type="checkbox" /> <label>Hiring a developer</label>
-        </li>
-      </ul>
-    </form>
-    <div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3530.265942400961!2d-82.665712684848!3d27.770778729308184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2e2315c782b2f%3A0xbf19abbc242dcd01!2s2220+Central+Ave%2C+St.+Petersburg%2C+FL+33712!5e0!3m2!1sen!2sus!4v1524147132170"
-        width="600"
-        height="450"
-        frameBorder="0"
-        allowFullscreen
-      />
+    <div className="wrap">
+      <h2>Contact Us</h2>
+      <p>Question? Comments? Just want to chat and say "Hello"?</p>
+      <form action="">
+        <fieldset>
+          <p>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </p>
+          <p>
+            <label htmlFor="">Email Address</label>
+            <input type="email" />
+          </p>
+          <p>
+            <label htmlFor="">Telephone Number</label>
+            <input type="tel" />
+          </p>
+          <p>
+            <label htmlFor="">Message</label>
+            <textarea name="" id="" cols="30" rows="10" />
+          </p>
+        </fieldset>
+        <fieldset>
+          <p>I'm interested in:</p>
+          <ul>
+            <li>
+              <input type="checkbox" /> <label>Learning web development</label>
+            </li>
+            <li>
+              <input type="checkbox" /> <label>Demo Day & hiring events</label>
+            </li>
+            <li>
+              <input type="checkbox" /> <label>Community events</label>
+            </li>
+            <li>
+              <input type="checkbox" />{' '}
+              <label>Donating or becoming a sponsor</label>
+            </li>
+            <li>
+              <input type="checkbox" /> <label>Volunteering</label>
+            </li>
+            <li>
+              <input type="checkbox" /> <label>Hiring a developer</label>
+            </li>
+          </ul>
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
+      <div />
     </div>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3530.265942400961!2d-82.665712684848!3d27.770778729308184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2e2315c782b2f%3A0xbf19abbc242dcd01!2s2220+Central+Ave%2C+St.+Petersburg%2C+FL+33712!5e0!3m2!1sen!2sus!4v1524147132170"
+      width="100%"
+      height="450"
+      frameBorder="0"
+      allowFullscreen
+    />
   </div>
 )
 
