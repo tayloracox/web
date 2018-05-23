26ec4fcfee635d5047907ce9e12cec57234dcc4e
Liz Tiller <lizthrilla@users.noreply.github.com>
2018-05-23 19:45:31 -0400

    kids image

--- src/pages/jr.js
+++ src/pages/jr.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import logoJunior from '../images/junior.svg'
 
 const JuniorPage = () => (
   <div className="JuniorPage">
-    <div className="wrap">
+    <img src={logoJunior} alt="Suncoast Developers Guild Jr" height="280" />
+    <h3>Suncoast Developers Guild Junior Code Classes</h3>
+    {/* <div className="wrap">
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScpmdobCYFKEmZsOdzS4To5kTs37zkchijZGh5oZAtKgnQMwg/viewform?embedded=true">
         Loading...
       </iframe>
-    </div>
+    </div> */}
   </div>
 )
 
