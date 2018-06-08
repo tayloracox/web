fcc599fbe3a9fd2f2fd4acaee67961bfc60f3873
Crystal <crystal.r.mcgowan@gmail.com>
2018-06-07 20:14:18 -0400

    Added Refer button to Academy Nav and built Refer page w Google form

---
+++ src/pages/academy/refer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AcademyNavigation from '../../components/AcademyNavigation'
+
+const AcademyReferPage = () => (
+  <div className="AcademyPage Refer">
+    <AcademyNavigation />
+    <div className="wrap">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSedmeKwrH-VH1dXkrHFQYF4NCQxwqnHMgicFdKi1awFwk55fg/viewform?embedded=true" width="100%">Loading...</iframe>
+    </div>
+  </div>
+)
+
+export default AcademyReferPage
