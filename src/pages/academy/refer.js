6aa501ff233a3dee3a6b93a3178026f6680ca9c5
Crystal <crystal.r.mcgowan@gmail.com>
2018-06-09 10:06:23 -0400

    removed width on refer page iframe

--- src/pages/academy/refer.js
+++ src/pages/academy/refer.js
@@ -5,7 +5,7 @@ const AcademyReferPage = () => (
   <div className="AcademyPage Refer">
     <AcademyNavigation />
     <div className="wrap">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSedmeKwrH-VH1dXkrHFQYF4NCQxwqnHMgicFdKi1awFwk55fg/viewform?embedded=true" width="100%">Loading...</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSedmeKwrH-VH1dXkrHFQYF4NCQxwqnHMgicFdKi1awFwk55fg/viewform?embedded=true">Loading...</iframe>
     </div>
   </div>
 )
