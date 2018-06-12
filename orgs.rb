7c0eda653b9318b0078db4efdea4adce6169d5b8
Jason L Perry <jason@suncoast.io>
2018-06-12 13:59:55 -0400

    Add ruby script for generating orgs
    This is temporary, we'll be switching to Netlify CMS for this soon.

---
+++ orgs.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'json'
+
+groups = %w[
+  tampa-rb
+  suncoast-js
+  Front-End-Design
+  NodeSchool-St-Petersburg
+  Girl-Develop-It-Tampa-Bay
+  Tampa-iOS-Meetup
+  St-Pete-NET-Meetup
+  tampa-nodejs
+  TampaUnity3D
+  WomenInLinux
+  suncoast-developers-guild
+  CoderNight
+  GeekLunch
+  Clearwater-Dunedin-Geek-Breakfast
+  GameCreators
+  python-178
+  Data-Scientists-Tampa-Bay
+  Sarasota-Web-Development-Meetup-Group
+  Code-Katas
+  ReactJS-Tampa-Bay
+  Saint-Petersburg-Python-Meetup
+  Tampa-Bay-WordPress
+  Laravel
+  Tampa-Bay-Android-Developers-Group
+  Blockchain-Enthusiasts
+  Design-St-Pete
+  Tampa-JUG
+  TBDevOps
+  Code-for-Tampa-Bay-Brigade
+].map do |urlname|
+  puts urlname
+  open("https://api.meetup.com/#{urlname}?key=#{ENV['MEETUP_API_KEY']}") do |io|
+    JSON.parse(io.read)
+  end
+end
+
+puts "\n\n"
+
+puts groups.to_json
