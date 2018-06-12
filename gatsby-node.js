71d2d5719a815bde545bcdb5f5cc8b19b94cec1c
Jason L Perry <jason@suncoast.io>
2018-06-12 13:38:41 -0400

    Add refer

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators
   return new Promise((resolve, reject) => {
-    if (page.path.match(/^\/(academy\/apply|jr|volunteer)/)) {
+    if (page.path.match(/^\/(academy\/(apply|refer)|jr|volunteer)/)) {
       page.layout = 'formEmbed'
       createPage(page)
     }
