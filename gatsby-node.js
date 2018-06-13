a42e0f03d1047960d3e63aa9b14c62415420b04a
Jason L Perry <jason@suncoast.io>
2018-06-13 14:25:31 -0400

    Fix a couple minor things with the kids page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators
   return new Promise((resolve, reject) => {
-    if (page.path.match(/^\/(academy\/(apply|refer)|jr|volunteer)/)) {
+    if (page.path.match(/^\/(academy\/(apply|refer)|volunteer)/)) {
       page.layout = 'formEmbed'
       createPage(page)
     }
