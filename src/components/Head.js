7f209e1a42866256f8f59e0730716e8d161f77b1
Jason L Perry <jason@suncoast.io>
2018-06-23 00:00:28 -0400

    Add social media meta tags

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+const DESCRIPTION = DESCRIPTION
+
 const Head = () => (
   <Helmet
     title="Suncoast Developers Guild"
     meta={[
-      { name: 'description', content: 'Suncoast Developers Guild' },
+      { name: 'description', content: DESCRIPTION },
       {
         name: 'keywords',
         content:
           'sdg, Tampa Bay, Code School, St Petersburg, Academy, Programming, Development, Suncoast',
       },
+      { name: 'description', content: DESCRIPTION },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:title', content: 'Suncoast Developers Guild' },
+      { name: 'twitter:description', content: DESCRIPTION },
+      { name: 'twitter:site', content: '@suncoastio' },
+      { name: 'twitter:creator', content: '@suncoastio' },
+      {
+        name: 'twitter:image:src',
+        content: 'https://suncoast.io/1024-512.png',
+      },
+      { name: 'og:title', content: 'Suncoast Developers Guild' },
+      { name: 'og:description', content: DESCRIPTION },
+      { name: 'og:image', content: 'https://suncoast.io/1200-630.png' },
+      { name: 'og:url', content: 'https://suncoast.io' },
+      { name: 'og:site_name', content: 'Suncoast Developers Guild' },
+      { name: 'og:locale', content: 'en_US' },
+      { name: 'fb:admins', content: '365336690280273' },
+      { name: 'fb:app_id', content: '228326264426970' },
+      { name: 'og:type', content: 'website' },
     ]}
     link={[
       {
