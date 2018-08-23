4a4b6ad2d7a4e28aa3518c52ce6d064e5d4084bd
Jason L Perry <jason@suncoast.io>
2018-08-23 15:21:52 -0400

    Update contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,86 +6,44 @@ const ContactPage = () => (
     <div className="wrap">
       <PageHeading>Contact Us</PageHeading>
       <p>Question? Comments? Just want to chat and say "Hello"?</p>
-      <form action="https://formspree.io/hello@suncoast.io" method="POST">
-        <input type="hidden" name="_next" value="https://suncoast.io/thanks" />
+      <form
+        action="https://app.nutshell.com/api/v1/public/lead/289162:17c8389f0262460d66b7c94e0902f3eb45bc56ca"
+        method="POST"
+      >
+        <input
+          type="hidden"
+          name="successUrl"
+          value="https://suncoast.io/thanks"
+        />
         <fieldset className="contact">
           <p>
             <label htmlFor="">Name</label>
-            <input type="text" name="name" />
+            <input type="text" name="contact[name]" />
           </p>
           <p>
             <label htmlFor="">Email Address</label>
-            <input type="email" name="email" />
+            <input type="email" name="contact[email]" />
           </p>
           <p>
             <label htmlFor="">Telephone Number</label>
-            <input type="tel" name="phone" />
+            <input type="tel" name="contact[phone]" />
           </p>
           <p>
             <label htmlFor="">Message</label>
-            <textarea name="message" id="" cols="30" rows="10" />
+            <textarea name="note" id="" cols="30" rows="10" />
           </p>
         </fieldset>
         <fieldset className="interests">
-          <p>I'm interested in:</p>
+          <p>Are you interested in:</p>
           <ul>
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name="interests"
-                  value="Learning web development"
-                />
-                Learning web development
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name="interests"
-                  value="Demo Day & hiring events"
-                />
-                Demo Day & hiring events
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name="interests"
-                  value="Community events"
-                />
-                Community events
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name="interests"
-                  value="Donating or becoming a sponsor"
-                />
-                Donating or becoming a sponsor
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" name="interests" value="Volunteering" />
-                Volunteering
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name="interests"
-                  value="Hiring a developer"
-                />
-                Hiring a developer
-              </label>
-            </li>
+            <li>Learning web development?</li>
+            <li>Demo Day & hiring events?</li>
+            <li>Community events?</li>
+            <li>Donating or becoming a sponsor?</li>
+            <li>Volunteering?</li>
+            <li>Hiring a developer?</li>
           </ul>
+          <p>Let us know!</p>
           <p className="action">
             <button type="submit">Submit</button>
           </p>
