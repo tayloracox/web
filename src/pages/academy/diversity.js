47c2382cdd1be87236e09c9f231e567bab0d342d
Jason L Perry <jason@suncoast.io>
2018-10-22 13:49:47 -0400

    Submit

--- src/pages/academy/diversity.js
+++ src/pages/academy/diversity.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import config from '../../../gatsby-config'
 import banner from '../../images/banner.svg'
 import PageHeading from '../../components/PageHeading'
 import AcademyNavigation from '../../components/AcademyNavigation'
 
+const GATEWAY_API_URL = config.siteMetadata.apis.gateway
+
 class DiversityScholarship extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    applied: false,
+    essay: '',
+    complete: false,
+  }
+
+  _update = event => {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  _submit = event => {
+    event.preventDefault()
+    fetch(GATEWAY_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify({
+        program: 'diversity_scholarship',
+        full_name: this.state.name,
+        email_address: this.state.email,
+        phone_number: this.state.phone,
+        question_responses: {
+          'I have applied for a program at the Academy at Suncoast Developers Guild.': this
+            .state.applied,
+          'Why is diversity in technology important to you?': this.state.essay,
+        },
+      }),
+    }).then(() => {
+      this.setState({ complete: true })
+    })
+  }
+
   render() {
+    if (this.state.complete) {
+      return <Redirect to="/thanks" />
+    }
     return (
       <div className="AcademyPage DiversityScholarship">
         <AcademyNavigation />
@@ -31,25 +79,45 @@ class DiversityScholarship extends Component {
                 </li>
               </ul>
             </div>
-            <form className="form">
+            <form className="form" onSubmit={this._submit}>
               <fieldset className="primary-fields">
                 <p>
                   <label>Full Name</label>
-                  <input type="text" name="full_name" />
+                  <input
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this._update}
+                  />
                 </p>
                 <p>
                   <label>Email</label>
-                  <input type="email" name="email" />
+                  <input
+                    type="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this._update}
+                  />
                 </p>
                 <p>
                   <label>Phone Number</label>
-                  <input type="tel" name="phone" />
+                  <input
+                    type="tel"
+                    name="phone"
+                    value={this.state.phone}
+                    onChange={this._update}
+                  />
                 </p>
               </fieldset>
               <fieldset className="secondary-fields">
                 <p className="confirm">
                   <label>
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      name="applied"
+                      checked={this.state.applied}
+                      onChange={this._update}
+                    />
                     <span>
                       I have applied for a program at the{' '}
                       <strong>Academy at Suncoast Developers Guild</strong>.
@@ -60,7 +128,11 @@ class DiversityScholarship extends Component {
                   <label>
                     Why is diversity in technology important to <em>you</em>?
                   </label>
-                  <textarea />
+                  <textarea
+                    name="essay"
+                    value={this.state.essay}
+                    onChange={this._update}
+                  />
                 </p>
               </fieldset>
               <p className="notice">
