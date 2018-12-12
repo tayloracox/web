f8ea4f6ea3d6ad1070fcde1729b06a3782a5be5d
Jason L Perry <jason@suncoast.io>
2018-12-12 15:56:36 -0500

    Add program schedule to Academy page, closes #76

--- src/pages/academy/index.js
+++ src/pages/academy/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import Tab from '../../components/Tab'
 import AcademyNavigation from '../../components/AcademyNavigation'
 import PageHeading from '../../components/PageHeading'
@@ -16,6 +17,66 @@ const AcademyPage = () => (
         <Tab to="/academy/career">Career Services</Tab>
       </ul>
 
+      <h3>Program Schedule</h3>
+
+      <table className="ProgramSchedule">
+        <thead>
+          <tr>
+            <th>Cohort</th>
+            <th>Status</th>
+            <th>Classes Start</th>
+            <th>Graduation / Demo Day</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="current">
+            <td>Cohort XII</td>
+            <td>
+              <em>In Progress</em>
+            </td>
+            <td>
+              October 22
+              <sup>nd</sup>, 2018
+            </td>
+            <td>
+              January 25
+              <sup>th</sup>, 2018
+            </td>
+          </tr>
+          <tr className="upcoming">
+            <td>Cohort XIII</td>
+            <td>
+              <Link to="/academy/apply">Now Enrolling</Link>
+            </td>
+            <td>
+              January 28
+              <sup>th</sup>, 2019
+            </td>
+            <td>
+              April 19
+              <sup>th</sup>, 2019
+            </td>
+          </tr>
+          <tr className="future">
+            <td>Cohort XIV</td>
+            <td>
+              <Link to="/academy/apply">Accepting Applicants</Link>
+            </td>
+            <td>
+              April 28
+              <sup>th</sup>, 2019
+            </td>
+            <td>TBA</td>
+          </tr>
+          <tr className="distant future">
+            <td>Cohort XV</td>
+            <td>&quot;</td>
+            <td>Summer 2019</td>
+            <td>TBA</td>
+          </tr>
+        </tbody>
+      </table>
+
       <h3>Our Curriculum</h3>
 
       <p>
@@ -26,6 +87,7 @@ const AcademyPage = () => (
         web applications (i.e. product work), or with agencies that produce
         websites and web applications for other companies (i.e. client work).
       </p>
+
       <p>
         Students will learn client-side (front-end) technology: HTML, CSS, and
         JavaScript as well as server-side (back-end) technology such as .NET or
