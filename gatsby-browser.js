46258413c9d813550ee5277b668bc19f8ce110c1
Jason L Perry <jason@suncoast.io>
2019-01-16 16:01:33 -0500

    Update
    A lazy commit.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,11 @@ exports.onClientEntry = () => {
   require.ensure(['@sentry/browser'], function(require) {
     const Sentry = require('@sentry/browser')
     Sentry.init({
-      dsn: process.env.SENTRY_DSN_URL,
+      dsn: process.env.GATSBY_SENTRY_DSN_URL,
       environment: process.env.NODE_ENV,
       enabled: () => process.env.NODE_ENV === 'production',
     })
-    console.log(process.env.SENTRY_DSN_URL)
+    console.log(process.env.NODE_ENV, process.env.GATSBY_SENTRY_DSN_URL)
     window.Sentry = Sentry
   })
 }
